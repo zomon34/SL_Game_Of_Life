471e8e24bd821b8284ee3301f5942f9052abd211
zomon34 <90390333+zomon34@users.noreply.github.com>
2023-09-28 09:50:50 +0200

    started on CheckLifeConditions function
    IT CURRECNTLY DOES NOT WORK CORRECTLY, I'm pushing just to have the "first" version saved somewhere

--- Assets/Cell.cs
+++ Assets/Cell.cs
@@ -1,15 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
     public bool alive;
+    public bool aliveNextStep;
     SpriteRenderer spriteRenderer;
 
     public void UpdateStatus()
     {
         spriteRenderer ??= GetComponent<SpriteRenderer>();
         spriteRenderer.enabled = alive;
+    }
+
+    public void Select() //Debug purposes that's used to see what cell is being tested with
+    {
+        spriteRenderer ??= GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Color.red;
     }
 }
--- Assets/GameOfLife.cs
+++ Assets/GameOfLife.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameOfLife : MonoBehaviour
@@ -8,7 +6,7 @@ public class GameOfLife : MonoBehaviour
     Cell[,] cells; //Maybe change to [][], because I want to try that.
     float cellSize = 0.25f;
     int numberOfColumns, numberOfRows;
-    int spawnChancePercentage = 15;
+    int spawnChancePercentage = 66;
     float width;
     float height;
 
@@ -27,8 +25,7 @@ public class GameOfLife : MonoBehaviour
 
         for (int y = 0; y < numberOfRows; y++)
         {
-            
-            for ( int x = 0; x < numberOfColumns; x++)
+            for (int x = 0; x < numberOfColumns; x++)
             {
                 Vector2 newPos = new Vector2(x * cellSize - width, y * cellSize - height);
 
@@ -57,8 +54,38 @@ public class GameOfLife : MonoBehaviour
         {
             for (int x = 0; x < numberOfColumns; x++)
             {
+                if (x == 4 && y == 4) // Debug purposes
+                {
+                    cells[x, y].alive = true; // Just so that the testing cell is always visible
+                    cells[x, y].Select();
+                    CheckLifeConditions(x, y); // Called to run any prints inside the function
+                }
                 cells[x, y].UpdateStatus();
             }
         }
+    }
+
+    bool CheckLifeConditions(int x, int y)
+    {
+        int aliveNeighbours = 0;
+
+        for (int a = y - 1; a < y + 1; a++)
+        {
+            for (int b = x - 1; b < x + 1; b++)
+            {
+                if (a != y && b != x)
+                {
+                    if (cells[b, a].alive)
+                    {
+                        aliveNeighbours++;
+                    }
+                }
+            }
+        }
+        if (aliveNeighbours > 3 || aliveNeighbours < 2)
+        {
+            return false;
+        }
+        return true;
     }
 }
